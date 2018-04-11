c72fa3f0edce3ac2cffd09293369aa4fc2d499bf
Thomas Wilburn <news@thomaswilburn.net>
2018-04-11 15:44:37 -0700

    Questions one-by-one

--- src/js/main.js
+++ src/js/main.js
@@ -2,13 +2,14 @@
 // var track = require("./lib/tracking");
 
 var $ = require("./lib/qsa");
-
+var closest = require("./lib/closest");
 
 var clickedOption = function(e) {
   if (this.classList.contains("answered")) return;
   if (e.target.tagName != "BUTTON") return;
   $("button", this).forEach(b => b.disabled = true);
-  this.classList.add("answered");
+  var question = closest(this, ".question");
+  question.classList.add("answered");
   e.target.classList.add("chosen");
   var answer = this.nextElementSibling;
   answer.classList.add("show");
