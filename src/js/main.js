6b442c469ed47372c019fe76f5833435dd0a4bf0
Kirsten O'Brien <37599020+kobrienST@users.noreply.github.com>
2018-04-16 16:38:44 -0700

    Simplify event listeners

--- src/js/main.js
+++ src/js/main.js
@@ -5,14 +5,11 @@ var $ = require("./lib/qsa");
 var closest = require("./lib/closest");
 
 var clickedOption = function(e) {
-  if (this.classList.contains("answered")) return;
   if (e.target.tagName != "BUTTON") return;
   $("button", this).forEach(b => b.disabled = true);
   var question = closest(this, ".question");
   question.classList.add("answered");
   e.target.classList.add("chosen");
-  var answer = this.nextElementSibling;
-  answer.classList.add("show");
 }
 
 $(".options").forEach(o => o.addEventListener("click", clickedOption));
