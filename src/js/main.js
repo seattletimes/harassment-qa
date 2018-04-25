3a64b7adaf28f4e868014f15ada1aa924f0c21b1
Thomas Wilburn <news@thomaswilburn.net>
2018-04-25 15:10:04 -0700

    Added question memory and hint

--- src/js/main.js
+++ src/js/main.js
@@ -4,12 +4,24 @@
 var $ = require("./lib/qsa");
 var closest = require("./lib/closest");
 
+var hash = window.location.hash || "#0";
+hash = hash.replace(/#/, "") * 1;
+
+var questions = $(".question");
+for (var i = 0; i < hash; i++) {
+  var q = questions[i];
+  q.classList.add("answered");
+  $("button", q).forEach(b => b.disabled = true);
+}
+
 var clickedOption = function(e) {
   if (e.target.tagName != "BUTTON") return;
   $("button", this).forEach(b => b.disabled = true);
   var question = closest(this, ".question");
   question.classList.add("answered");
   e.target.classList.add("chosen");
+  window.location.hash = ++hash;
 }
 
 $(".options").forEach(o => o.addEventListener("click", clickedOption));
+
